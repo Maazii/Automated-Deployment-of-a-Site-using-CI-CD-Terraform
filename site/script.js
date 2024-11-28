02a1836caf7fc8fc3662084fa6110a86679f00da
Maazii <maaz_rehman39@hotmail.com>
2024-11-28 18:27:21 +0500

    Uploading base

---
+++ site/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent form from submitting
+    alert('Thank you for reaching out!');
+});
